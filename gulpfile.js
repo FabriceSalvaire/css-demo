09c1bbcacff0ae53c84c19ddc536f90af6f446b0
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2016-09-27 11:58:29 +0200

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,88 +1,76 @@
-'use strict';
-
-// #############################################################################
-// IMPORTS
-var webdriverUpdate;
-var argv = require('minimist')(process.argv.slice(2));
 var gulp = require('gulp');
+var bower = require('gulp-bower');
+var browser_sync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var reload = browser_sync.reload;
+var exec = require('child_process').exec;
 
-// #############################################################################
-// SETTINGS
-var PROJECT_ROOT = __dirname;
-var PROJECT_PATH = {
-    bower: PROJECT_ROOT + '/static/vendor',
-    css: PROJECT_ROOT + '/static/css',
-    fonts: PROJECT_ROOT + '/static/fonts',
-    html: PROJECT_ROOT + '/templates',
-    images: PROJECT_ROOT + '/static/img',
-    icons: PROJECT_ROOT + '/private/icons',
-    js: PROJECT_ROOT + '/static/js',
-    sass: PROJECT_ROOT + '/private/sass',
+var minify_css = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
+// var sourcemaps = require('gulp-sourcemaps');
+// var gutil = require('gulp-util');
+// var gulpif = require('gulp-if');
+var header = require('gulp-header');
+
+var project_root = __dirname;
+
+var project_path = {
+    bower: project_root + '/static/vendor',
+    css: project_root + '/static/css',
+    html: project_root + '/html',
+    sass: project_root + '/private/sass',
+    template: project_root + '/template',
 };
 
-var PROJECT_PATTERNS = {
-    images: [
-        PROJECT_PATH.images + '/**/*',
-        // exclude from preprocessing
-        '!' + PROJECT_PATH.images + '/dummy/*/**'
-    ],
-    js: [
-        'gulpfile.js',
-        './tools/tasks/**/*.js',
-        PROJECT_PATH.js + '/**/*.js',
-        PROJECT_PATH.tests + '/**/*.js',
-        // exclude from linting
-        '!' + PROJECT_PATH.js + '/*.min.js',
-        '!' + PROJECT_PATH.js + '/**/*.min.js',
-        '!' + PROJECT_PATH.tests + '/coverage/**/*'
+var project_patterns = {
+    html: project_path.html + '/*.html',
+    template: [
+	project_root + '/run-jinja',
+	project_path.template + '/*.html'
     ],
     sass: [
-        PROJECT_PATH.sass + '/**/*.{scss,sass}'
+        project_path.sass + '/**/*.{scss,sass}'
     ]
 };
 
-var DEFAULT_PORT = 8000;
-var PORT = parseInt(process.env.PORT, 10) || DEFAULT_PORT;
-var DEBUG = argv.debug;
-
-
-/**
- * Checks project deployment
- * @param {String} id - task name
- * @returns {Object} - task which finished
- */
-function task (id) {
-    return require('./tools/tasks/' + id)(gulp, {
-        PROJECT_ROOT: PROJECT_ROOT,
-        PROJECT_PATH: PROJECT_PATH,
-        PROJECT_PATTERNS: PROJECT_PATTERNS,
-        DEBUG: DEBUG,
-        PORT: PORT
-    });
-}
-
-gulp.task('bower', task('bower'));
-gulp.task('lint:javascript', task('lint/javascript'));
-gulp.task('lint', ['lint:javascript']);
-gulp.task('sass', task('sass'));
-gulp.task('build', ['sass']);
-
-/**
- * GULP_MODE === 'production' means we have a limited
- * subset of tasks, namely sass, bower and lint to
- * speed up the deployment / installation process.
- */
-if (process.env.GULP_MODE !== 'production') {
-    gulp.task('images', task('images'));
-    gulp.task('preprocess', ['sass', 'images']);
-    gulp.task('icons', task('icons'));
-
-    gulp.task('browser', task('browser'));
-}
-
-gulp.task('watch', function () {
-    gulp.watch(PROJECT_PATTERNS.sass, ['sass']);
-    gulp.watch(PROJECT_PATTERNS.js, ['lint']);
+gulp.task('bower', function() {
+    bower(gulp.dest(opts.PROJECT_PATH.bower));
 });
 
-gulp.task('default', ['bower', 'sass', 'lint', 'watch']);
+gulp.task('jinja', function (cb) {
+  exec(project_root + '/run-jinja', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
+})
+
+// Compile sass into CSS
+gulp.task('sass', function() {
+    return gulp.src(project_patterns.sass)
+        .pipe(sass())
+        .pipe(autoprefixer({
+            // browsers are coming from browserslist file
+            cascade: false
+        }))
+    // .pipe(minify_css({
+    //     rebase: false
+    // }))
+        .pipe(header('/* This file generated automatically on server side. All changes would be lost. */ \n\n'))
+        .pipe(gulp.dest(project_path.css))
+        .pipe(reload({stream: true}));
+});
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass', 'jinja'], function() {
+
+    browser_sync.init({
+        server: project_path.html
+    });
+
+    gulp.watch(project_patterns.template, ['jinja']);
+    gulp.watch(project_patterns.sass, ['sass']);
+    gulp.watch(project_patterns.html).on('change', reload);
+});
+
+gulp.task('default', ['serve']);
